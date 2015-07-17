5d3f5fda9fa766ac10c8bdd34e440b81e54406f5
tangkikodo <allmonday@126.com>
2015-07-17 23:56:05 +0800

    add example of flask jwt

--- _logging/logger.py
+++ _logging/logger.py
@@ -1,4 +1,7 @@
 import logging
 import os
-logging.basicConfig(filename = os.path.join(os.getcwd(), 'log.txt'), level = logging.DEBUG)
+# logging.basicConfig(filename = os.path.join(os.getcwd(), 'log.txt'), level = logging.DEBUG)
+logging.basicConfig(level = logging.DEBUG,
+                    format="%(asctime)s %(levelname)s:%(message)s",
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
 logging.debug('this is a message')
